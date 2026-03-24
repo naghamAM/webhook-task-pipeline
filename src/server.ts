92d73fb1b94c30866481b67091da6a4f18a22a49
Nagham Amarneh <n.amarneh2017@gmail.com>
2026-03-24 22:09:34 +0200

    Fix app import for Docker build

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
